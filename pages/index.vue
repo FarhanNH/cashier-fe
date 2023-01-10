e35194a7894411542a25b37b0cce916ba8abae20
farhan.huda <farhan.huda@pactindo.com>
2023-01-10 09:07:47 +0700

    vuex

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,8 @@
 <template>
   <v-row>
     <v-col cols="8" class="frame-content px-6">
-      <products />
+      <product />
     </v-col>
     <v-col cols="4" class="frame-content white"> ORDERS </v-col>
   </v-row>
 </template>
-
-<script>
-import Products from '~/components/Products.vue'
-export default {
-  components: { Products },
-  name: 'IndexPage',
-}
-</script>
