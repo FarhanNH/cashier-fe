f12613149b752009c8e57a2ba493f0a787b36da0
farhan.huda <farhan.huda@pactindo.com>
2023-01-13 09:04:55 +0700

    add mapGetters

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,9 @@
     <v-col cols="8" class="frame-content px-6">
       <product />
     </v-col>
-    <v-col cols="4" class="frame-content white"> ORDERS </v-col>
+    <v-col cols="4" class="frame-content white">
+      ORDER
+      <order />
+    </v-col>
   </v-row>
 </template>
